c10761e4fdc3f99cd11c7634afc759d6767a5d8e
Amit Kanti Barua <amit.barua@kdsgroup.net>
2021-08-22 15:34:37 +0600

    First commit

---
+++ BootstrapModalPartialView/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using BootstrapModalPartialView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapModalPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ModalAction(int Id)
+        {
+            ViewBag.Id = Id;
+            return PartialView("ModalContent");
+        }
+
+        public ActionResult ModalFormAction(int Id)
+        {
+            ViewBag.Id = Id;
+            return PartialView("ModalFormContent");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PostModal()
+        {
+            var closeModal = new CloseModal
+            {
+                ShouldClose = true,
+                FetchData = true,
+                Destination = Url.Action("List"),
+                Target = "list",
+                OnSuccess = "success"
+            };
+
+            return PartialView("CloseModal", closeModal);
+        }
+    }
+}
---
+++ BootstrapModalPartialViewCore/Models/CloseModal.cs
@@ -0,0 +1,11 @@
+﻿namespace BootstrapModalPartialViewCore.Models
+{
+    public class CloseModal
+    {
+        public bool ShouldClose { get; set; }
+        public bool FetchData { get; set; }
+        public string Destination { get; set; }
+        public string Target { get; set; }
+        public string OnSuccess { get; set; }
+    }
+}
